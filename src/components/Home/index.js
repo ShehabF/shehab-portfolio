a75827acf2fc46d3c59858400e19d79c79b459c5
ShehabF <sfarooqui8@gatech.edu>
2023-09-21 10:32:02 -0400

    Home Page Logo and animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,42 @@
+import { Link } from "react-router-dom";
+import './index.scss';
+import { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+import Logo from "../Logo";
+
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray= ['S', 'h', 'e', 'h', 'a', 'b', ' ',
+                    'F', 'a', 'r', 'o', 'o', 'q', 'u', 'i']
+    const jobArray= ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ',
+                    'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r']
+    
+                    useEffect(() => {
+                        setTimeout(() => {
+                          setLetterClass('text-animate-hover')
+                        }, 4000)
+                      }, [])
+
     return(
         <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={nameArray}
+                    index={15}/>
 
+                    <br/>
+
+                    <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={jobArray}
+                    index={18}/>
+                </h1>
+                <h2> Frontend Developer/ UI/UX Design / Photography</h2>
+                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo/>
         </div>
     );
 }
